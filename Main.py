e5fc2256518b2cccb918e003863c77c66928c2e2
marou <ma_kacho@live.concordia.ca>
2024-03-11 22:12:37 -0400

    Committing data visualization code

--- Main.py
+++ Main.py
@@ -1,14 +1,14 @@
 from Data_Cleaning import clean_folder
 
-# Paths for our datasets: training set and testing set for all emotions: happy, neutral, surprise and focused
+# Paths for our datasets: training set and testing set for all emotions: happy, neutral, surprised and focused
 train_happy_path = "/Users/doghm/OneDrive/Documents/Concordia/Semester 6/COMP 472/Dataset/train/happy"
 train_neutral_path = "/Users/doghm/OneDrive/Documents/Concordia/Semester 6/COMP 472/Dataset/train/neutral"
-train_surprise_path = "/Users/doghm/OneDrive/Documents/Concordia/Semester 6/COMP 472/Dataset/train/surprise"
+train_surprise_path = "/Users/doghm/OneDrive/Documents/Concordia/Semester 6/COMP 472/Dataset/train/surprised"
 test_happy_path = "/Users/doghm/OneDrive/Documents/Concordia/Semester 6/COMP 472/Dataset/test/happy"
 test_neutral_path = "/Users/doghm/OneDrive/Documents/Concordia/Semester 6/COMP 472/Dataset/test/neutral"
-test_surprise_path = "/Users/doghm/OneDrive/Documents/Concordia/Semester 6/COMP 472/Dataset/test/surprise" 
+test_surprise_path = "/Users/doghm/OneDrive/Documents/Concordia/Semester 6/COMP 472/Dataset/test/surprised"
 
-# Clean all images for all emotions: happy, neutral, surprise and focused
+# Clean all images for all emotions: happy, neutral, surprised and focused
 clean_folder(train_happy_path)
 clean_folder(train_neutral_path)
 clean_folder(train_surprise_path)
