32d834c2e77f796d0be3df57e857b11f039615fc
marou <ma_kacho@live.concordia.ca>
2024-03-30 02:11:38 -0400

    Committing CNN file in progress

--- Part2-CNN-Algorithm/cnn_code.py
+++ Part2-CNN-Algorithm/cnn_code.py
@@ -36,6 +36,7 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 # next, we create the CNN model
+
 # class CNN(nn.Module):
 
 
