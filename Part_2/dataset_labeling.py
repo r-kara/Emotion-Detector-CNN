9fad592e8179e00368202a79df6b2de996b9b910
kara <racha.kara@gmail.com>
2024-04-02 01:50:16 -0400

    Update data_labeling
    Small changes

--- Part_2/dataset_labeling.py
+++ Part_2/dataset_labeling.py
@@ -2,12 +2,12 @@
 import os
 
 # Relative path
-train_folder = 'NewDataset/training'
-test_folder = 'NewDataset/testing'
-val_folder = 'NewDataset/validation'
+train_folder = 'Part_2/NewDataset/training'
+test_folder = 'Part_2/NewDataset/testing'
+val_folder = 'Part_2/NewDataset/validation'
 
-# Function to rename files with sequential numbering
-def rename_with_sequential_numbering(folder_path, class_name):
+# Rename files with sequential numbering
+def rename_files(folder_path, class_name):
     counter = 1
     for filename in sorted(os.listdir(folder_path)):
         base, ext = os.path.splitext(filename)
@@ -21,16 +21,16 @@ def rename_with_sequential_numbering(folder_path, class_name):
         new_name = f'{class_name}_{base}{ext}'
         os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_name))
 
-# Iterate over the folders containing each class
+# Iterate over each class of emotion
 for class_folder in ('engaged', 'happy', 'neutral', 'surprised'):
     # train dataset
     train_class_folder = os.path.join(train_folder, class_folder)
-    rename_with_sequential_numbering(train_class_folder, 'train_' + class_folder)
+    rename_files(train_class_folder, 'train_' + class_folder)
 
     # test dataset
     test_class_folder = os.path.join(test_folder, class_folder)
-    rename_with_sequential_numbering(test_class_folder, 'test_' + class_folder)
+    rename_files(test_class_folder, 'test_' + class_folder)
     
     # validation dataset
     val_class_folder = os.path.join(val_folder, class_folder)
-    rename_with_sequential_numbering(val_class_folder, 'val_' + class_folder)
+    rename_files(val_class_folder, 'val_' + class_folder)
